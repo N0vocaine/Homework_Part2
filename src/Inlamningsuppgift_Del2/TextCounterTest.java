14d537ed7dba01af335e745790dc5c2f3c2c6dfa
adria <adrianam.bucur@gmail.com>
2024-11-04 14:11:08 +0100

    Initial commit

---
+++ src/Inlamningsuppgift_Del2/TextCounterTest.java
@@ -0,0 +1,34 @@
+package Inlamningsuppgift_Del2;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class TextCounterTest {
+
+    @Test
+    public void testNormalInput() {
+        TextCounter counter = new TextCounter();
+        counter.addLine("Hello World");
+        counter.addLine("Java Programming");
+
+        assertEquals(2, counter.getLineCount(), "Line count should be 2");
+        assertEquals(27, counter.getCharCount(), "Character count should be 24");
+    }
+    @Test
+    public void testEmptyInput() {
+        TextCounter counter = new TextCounter();
+
+        assertEquals(0, counter.getLineCount(), "Line count should be 0 for no input");
+        assertEquals(0, counter.getCharCount(), "Character count should be 0 for no input");
+    }
+    @Test
+    public void testMixedInputLength() {
+        TextCounter counter = new TextCounter();
+        counter.addLine("Short");
+        counter.addLine("This is a longer line of text.");
+        counter.addLine("");
+
+        assertEquals(3, counter.getLineCount(), "Line count should match the number of lines added");
+        assertEquals(35, counter.getCharCount(), "Character count should reflect total characters in non-empty lines");
+    }
+}
